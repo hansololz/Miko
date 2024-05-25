f72a67ed772e4fe18adb573cca9a1454a47c1372
hansololz <david.han.zhang@gmail.com>
2024-05-25 16:34:39 -0700

    add move code for menu view

--- Miko/Views/MenuView.swift
+++ Miko/Views/MenuView.swift
@@ -6,12 +6,61 @@ struct MenuView: View {
     @Binding var searchText: String
     @Binding var sheetOffset: CGFloat
     
-    var body: some View {
-        VStack {
-            Text("Menu Text")
-                .multilineTextAlignment(.center)
-                .background(Color.white)
-                .padding(.all, 20)
+    @State private var searchEngineOption: String? = "Google"
+    
+    private var appVersion: String {
+            if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+                return version
+            } else {
+                return "1.0"
+            }
         }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("App Version: \(appVersion)")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    NavigationLink(destination: Text("Privacy Policy")) {
+                        Label("Privacy Policy", systemImage: "doc")
+                    }
+                    NavigationLink(destination: Text("Contact")) {
+                        Label("Contact", systemImage: "envelope")
+                    }
+                }
+                
+                Section(header: Text("Search Engine")) {
+                    getSearchEngineOption(option: "Google")
+                    getSearchEngineOption(option: "Bing")
+                    getSearchEngineOption(option: "DuckDuckGo")
+                }
+            }
+            .navigationTitle("Neko Cam")
+            .navigationBarItems(leading: Button("Done") {
+                selectSheetAnchor = restSheetAnchor
+            })
+        }
+    }
+    
+    private func getSearchEngineOption(option: String) -> some View {
+        Button(action: {
+            searchEngineOption = option
+        }) {
+            HStack {
+                Text(option)
+                Spacer()
+                if searchEngineOption == option {
+                    Image(systemName: "checkmark")
+                        .foregroundColor(.blue)
+                }
+            }
+        }
+        .background(Color.white)
+        .cornerRadius(10)
     }
 }
