0c7b634af0c54effa545a2d5a399393c7271302a
hansololz <david.han.zhang@gmail.com>
2024-05-25 18:59:19 -0700

    added contact page

--- Miko/Views/MenuView.swift
+++ Miko/Views/MenuView.swift
@@ -38,7 +38,7 @@ struct MenuView: View {
                     NavigationLink(destination: PrivacyPolicyView()) {
                         Label("Privacy Policy", systemImage: "doc")
                     }
-                    NavigationLink(destination: Text("Contact")) {
+                    NavigationLink(destination: ContactView()) {
                         Label("Contact", systemImage: "envelope")
                     }
                 }
@@ -101,29 +101,40 @@ struct MenuView: View {
 
 struct PrivacyPolicyView: View {
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("App Data")) {
-                    Text("The app itself, excluding the search engines, does not store any data externally. App settings data are only stored on device.")
-                }
-                
-                Section(header: Text("Third Party Search Engines")) {
-                    Text("Third-party search engines used by this app may store data in browser cookies or externally. This data is controlled solely by the search engines, and you should follow their terms of use and privacy policy.\n\nThe app displays third party search engines as webpages in a browser view.")
-                }
-
-                Section(header: Text("Changes to This Privacy Policy")) {
-                    Text("We may update our privacy policy from time to time. We will notify you of any changes by posting the new privacy policy on this page.")
-                }
-                
-                Section(header: Text("Acceptance of Terms")) {
-                    Text("By using our app, you signify your acceptance of this privacy policy. If you do not agree to this policy, please do not use our app. Your continued use of the app following the posting of changes to this policy will be deemed your acceptance of those changes.")
-                }
-                
-                Section(header: Text("Contact Us")) {
-                    Text("If you have any questions about this privacy policy, please contact us:\n- By email: nekocam@deezus.com")
-                }
+        List {
+            Section(header: Text("App Data")) {
+                Text("The app itself, excluding the search engines, does not store any data externally. App settings data are only stored on device.")
             }
-            .navigationBarTitle("Privacy Policy")
+            
+            Section(header: Text("Third Party Search Engines")) {
+                Text("Third-party search engines used by this app may store data in browser cookies or externally. This data is controlled solely by the search engines, and you should follow their terms of use and privacy policy.\n\nThe app displays third party search engines as webpages in a browser view.")
+            }
+            
+            Section(header: Text("Changes to This Privacy Policy")) {
+                Text("We may update our privacy policy from time to time. We will notify you of any changes by posting the new privacy policy on this page.")
+            }
+            
+            Section(header: Text("Acceptance of Terms")) {
+                Text("By using our app, you signify your acceptance of this privacy policy. If you do not agree to this policy, please do not use our app. Your continued use of the app following the posting of changes to this policy will be deemed your acceptance of those changes.")
+            }
+            
+            Section(header: Text("Contact Us")) {
+                Text("If you have any questions about this privacy policy, please contact us: -By email: nekocam@deezus.com")
+                    .textSelection(.enabled)
+            }
         }
+        .navigationBarTitle("Privacy Policy")
+    }
+}
+
+struct ContactView: View {
+    var body: some View {
+        List {
+            Section() {
+                Text("If you have any feedback, feature requests, or want to report an issue, please contact us: -By email: nekocam@deezus.com\n\nPlease do not include any personal information. If you are reporting an issue, please help us by including the steps to reproduce the issue.")
+                    .textSelection(.enabled)
+            }
+        }
+        .navigationBarTitle("Contact")
     }
 }
