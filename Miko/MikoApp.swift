8ec8a796331221e79a06eb4dac6c9100b7c89d05
hansololz <david.han.zhang@gmail.com>
2024-05-27 11:10:50 -0700

    removed unused data set

--- Miko/MikoApp.swift
+++ Miko/MikoApp.swift
@@ -5,7 +5,7 @@ import SwiftData
 struct MikoApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+//            Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
         
